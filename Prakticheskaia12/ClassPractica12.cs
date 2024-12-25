2e0b08ae946855697ec673f548e9add364fe4fb4
Артём Жаров <artemzharov123456@outlook.com>
2024-12-25 15:36:35 +0300

    Добавьте файлы проекта.

---
+++ Prakticheskaia12/ClassPractica12.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace Prakticheskaia12
+{
+    class Zadanie1
+    {
+        public double katet1;
+        public double katet2;
+
+        public void SetParams(int one, int two)
+        {
+            katet1 = one;
+            katet2 = two;
+        }
+        public double Gipotenyza()
+        {
+            double result = Math.Sqrt(Math.Pow(katet1, 2) + Math.Pow(katet2, 2));
+            return result;
+        }
+    }
+    class Zadanie2
+    {
+        public int chislo;
+
+        private int Chislo
+        {
+            get
+            {
+                return chislo;
+            }
+            set
+            {
+                if (value > 999) chislo = value;
+                else MessageBox.Show("Введите число большее 999");
+            }
+        }
+        public void SetParams(int one)
+        {
+            Chislo = one;
+        }
+        public int RazradSoten()
+        {
+            int result = (chislo / 100) % 10;
+            return result;
+        }
+
+    }
+}
---
+++ Prakticheskaia12/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Prakticheskaia12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btZadanie1_Click(object sender, RoutedEventArgs e)
+        {
+            Zadanie1 zadanie1 = new Zadanie1();
+            bool boolKatet1 = int.TryParse(tbKatet1.Text, out int katet1);
+            bool boolKatet2 = int.TryParse(tbKatet2.Text, out int katet2);
+            if (boolKatet1 || boolKatet2)
+            {
+                zadanie1.SetParams(katet1, katet2);
+                double result = zadanie1.Gipotenyza();
+                tbResult.Text = Convert.ToString(result);
+                tbKatet1.Focus();
+            }
+            else MessageBox.Show("Что то не так");
+        }
+
+        private void btZadanie2_Click(object sender, RoutedEventArgs e)
+        {
+            Zadanie2 zadanie2 = new Zadanie2();
+            bool boolChislo = int.TryParse(tbChislo.Text, out int chislo);
+            if (boolChislo)
+            {
+                zadanie2.SetParams(chislo);
+                double result = zadanie2.RazradSoten();
+                tbResult2.Text = Convert.ToString(result);
+                tbChislo.Focus();
+            }
+            else MessageBox.Show("Что то точнно не так");
+        }
+
+        private void btInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Информация");
+        }
+
+        private void btExit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        DispatcherTimer timer;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.IsEnabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            time.Text = d.ToString("HH:mm");
+            data.Text = d.ToString("dd.MM.yyyy");
+        }
+        private void Katet_Changed(object sender, EventArgs e)
+        {
+            tbResult.Text = "";
+        }
+        private void Chislo_Changed(object sender, EventArgs e)
+        {
+            tbResult2.Text = "";
+        }
+    }
+}
